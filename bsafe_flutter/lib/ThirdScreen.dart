7dbe199f5592a737583c665f010b2b5a07cc67f5
callison98 <callison4@mail.csuchico.edu>
2020-03-11 13:24:27 -0700

    added basic map

--- bsafe_flutter/lib/ThirdScreen.dart
+++ bsafe_flutter/lib/ThirdScreen.dart
@@ -1,14 +1,55 @@
 // ThirdScreen.dart -- Chris / Logan
 
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+
 
 class ThirdScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Text('Chris\'s \\ Logan\'s Page'),
+        child: MyMap(title: 'BSafe'),
       ),
     );
   }
 }
+
+class MyMap extends StatefulWidget {
+  MyMap({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyMapState createState() => _MyMapState();
+}
+
+class _MyMapState extends State<MyMap> {
+  GoogleMapController mapController;
+  //*** Coordinates for chico
+  final LatLng _center = const LatLng(37.4219983, -122.084);
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title:Center(
+            child:Text('Map'),
+          ),
+          backgroundColor: Colors.deepPurple,
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
