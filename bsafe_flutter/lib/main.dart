927d2cbd2b6f6095bfbf10206ff947affa9748d7
ssmathistad <ssmathistad@csuchico.edu>
2020-03-16 22:31:04 -0700

    Removed debug banner

--- bsafe_flutter/lib/main.dart
+++ bsafe_flutter/lib/main.dart
@@ -45,6 +45,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: appTitle,
       home: MyHomePage(title: appTitle),
     );
