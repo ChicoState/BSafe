1075ffd065b50d86dfc3d5eb83173453a58e45b5
ssmathistad <ssmathistad@csuchico.edu>
2020-04-23 12:38:21 -0700

    Added initial test

--- bsafe_flutter/test/widget_test.dart
+++ bsafe_flutter/test/widget_test.dart
@@ -9,6 +9,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:BSafe/main.dart';
 
+void main() {
+  testWidgets('app should work', (tester) async {
+    await tester.pumpWidget(new MyApp());
+    expect(find.text('Main Page'), findsOneWidget);
+  });
+}
+
 /*
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
