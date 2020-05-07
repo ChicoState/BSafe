2c8126624a87cdcd5a7f9d9a306a30bc6d1cc511
ssmathistad <ssmathistad@csuchico.edu>
2020-05-07 04:09:08 -0700

    Added test for verifying authentication state

---
+++ test/auth_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import '../lib/auth/auth.dart';
+
+void main() {
+
+  testWidgets('Verify transition to authetication state', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MaterialApp(
+      home: Authenticate(),
+    ));
+
+    expect(find.text('Sign in anonymously'), findsOneWidget);
+  });
+
+}
