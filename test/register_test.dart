1b57b852341e6c68eb9804ccefcf9283102428e8
ssmathistad <ssmathistad@csuchico.edu>
2020-05-07 04:00:59 -0700

    Adding test for registration

--- test/register_test.dart
+++ test/register_test.dart
@@ -11,5 +11,13 @@ import 'package:flutter/material.dart';
 
 void main() {
 
+  testWidgets('Verify transition to register page', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MaterialApp(
+      home: Register(),
+    ));
+
+    expect(find.text('Sign in anonymously'), findsOneWidget);
+  });
 
 }
